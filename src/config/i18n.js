f20872bf682824132376af07f86c1d134ae1f993
Ilan Zaro <ilanzaro@gmail.com>
2023-11-17 13:11:11 +0200

    i18n init resources

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,7 +1,7 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 import { LANGUAGES } from "../constants";
 
 i18n
@@ -9,20 +9,28 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(HttpApi) //for serving secific translation files
   .init({
+    resources: {
+      en: {
+        translation: require("../../public/assets/locales/en/translation.json"),
+      },
+      he: {
+        translation: require("../../public/assets/locales/he/translation.json"),
+      },
+    },
     debug: true,
     //lng: document.querySelector('html').lang, // if you're using a language detector, do not define the lng option
-    supportedLngs: LANGUAGES.map((lang)=> lang.code),
-    fallbackLng: 'en',
-    detection: { 
-        order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
-        caches: ['cookie']
+    supportedLngs: LANGUAGES.map((lang) => lang.code),
+    fallbackLng: "en",
+    detection: {
+      order: ["htmlTag", "cookie", "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
     },
     backend: {
-        loadPath: '/assets/locales/{{lng}}/translation.json',
+      loadPath: "/assets/locales/{{lng}}/translation.json",
     },
     react: {
-        useSuspense: false,
-      },
+      useSuspense: false,
+    },
   });
 
-  export default i18n;
+export default i18n;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./config/i18n";
+//import "./config/i18n";
 import App from "./App";
 import store from "./redux/configureStore";
 import { BrowserRouter } from "react-router-dom";
@@ -15,9 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
+        <App />
       </BrowserRouter>
     </StrictMode>
   </Provider>,
