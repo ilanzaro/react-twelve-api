c4334afa66e9f323c941e8874981ef4062f61ce5
Ilan Zaro <ilanzaro@gmail.com>
2023-11-17 08:00:11 +0200

    i18n

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -5,10 +5,11 @@ import HttpApi from 'i18next-http-backend';
 import { LANGUAGES } from "../constants";
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector) // browser language detector as a middleware
+  .use(initReactI18next) // passes i18n down to react-i18next
   .use(HttpApi) //for serving secific translation files
   .init({
+    debug: true,
     //lng: document.querySelector('html').lang, // if you're using a language detector, do not define the lng option
     supportedLngs: LANGUAGES.map((lang)=> lang.code),
     fallbackLng: 'en',
